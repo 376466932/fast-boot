f6747285c64b64de7261328d39097cb02d2860ba
zenghui liu <zenghui.liu1@pacteraedge.com>
2022-04-29 16:08:10 +0800

    init

---
+++ src/main/java/com/fast/fastboot/FastBootApplication.java
@@ -0,0 +1,13 @@
+package com.fast.fastboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastBootApplication.class, args);
+	}
+
+}
